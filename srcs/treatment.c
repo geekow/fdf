83d6447b5285835c87448fb8b615610c04e5b9c3
User <user@Users-Mac.local>
2017-10-23 17:05:54 +0200

    Working for the moment

--- srcs/treatment.c
+++ srcs/treatment.c
@@ -16,7 +16,7 @@ void	put_pixel(int x, int y, int color, t_fdf *fdf)
 {
 	int	position;
 
-	if (x < fdf->window_x && y < fdf->window_y)
+	if (x >= 0 && x < fdf->window_x && y >= 0 && y < fdf->window_y)
 	{
 		position = (fdf->img_size_line * y) + (x * fdf->img_octet_per_pixel);
 		ft_strncpy(fdf->imgdata + position, (char*)&color,
@@ -24,9 +24,31 @@ void	put_pixel(int x, int y, int color, t_fdf *fdf)
 	}
 }
 
+void	apply_y(t_coord *coord, int y, t_fdf *fdf)
+{
+	coord->x = 50 + (y * fdf->zoom);
+	coord->y = 100 - (fdf->zoom * y);
+}
+
+void	apply_x(t_coord *coord, int x, t_fdf *fdf)
+{
+	coord->x += 50 + (x * fdf->zoom);
+	coord->y += 50 + (x * fdf->zoom);
+}
+
+void	apply_z(t_coord *coord, int z, t_fdf *fdf)
+{
+	coord->y -= z + fdf->zoom;
+}
+
 void	trace_point(t_fdf *fdf, t_coord *coord)
 {
-	put_pixel(coord->x * fdf->zoom, coord->y * fdf->zoom, 0xFFFFFF, fdf);
+	t_coord	result;
+
+	apply_y(&result, coord->y, fdf);
+	apply_x(&result, coord->x, fdf);
+	apply_z(&result, coord->z, fdf);
+	put_pixel(result.x, result.y, 0xFFFFFF, fdf);
 }
 
 void	treatment(t_fdf *fdf)
