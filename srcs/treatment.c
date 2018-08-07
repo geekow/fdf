bf459c2a2dba9ee63b6760e378dd68c8db3905d8
Jean Jacobi <jjacobi@student.42.fr>
2018-08-07 17:43:46 +0200

    WIP

--- srcs/treatment.c
+++ srcs/treatment.c
@@ -25,8 +25,116 @@ void	put_pixel(int x, int y, int color, t_fdf *fdf)
 	}
 }
 
+void trace_line(t_xy *one, t_xy *two, t_fdf *fdf, int color)
+{
+	int dx;
+	int dy;
+	int i;
+	int xinc;
+	int yinc;
+	int cumul;
+	int x;
+	int y;
+
+	x = one->x;
+	y = one->y;
+	dx = two->x - one->x;
+	dy = two->y - one->y;
+	xinc = ( dx > 0 ) ? 1 : -1 ;
+	yinc = ( dy > 0 ) ? 1 : -1 ;
+	dx = abs(dx) ;
+	dy = abs(dy) ;
+	put_pixel(x, y, color, fdf) ;
+	if ( dx > dy )
+	{
+		cumul = dx / 2 ;
+		for ( i = 1 ; i <= dx ; i++ )
+		{
+			x += xinc ;
+			cumul += dy ;
+			if ( cumul >= dx )
+			{
+				cumul -= dx ;
+				y += yinc ;
+			}
+		put_pixel(x, y, color, fdf);
+		}
+	}
+	else
+	{
+	  cumul = dy / 2 ;
+	  for ( i = 1 ; i <= dy ; i++ )
+	  {
+		  y += yinc ;
+		  cumul += dx ;
+		  if (cumul >= dy)
+		  {
+			  cumul -= dy;
+			  x += xinc;
+		  }
+		put_pixel(x, y, color, fdf);
+	}
+	}
+  }
+
+void	trace_line_broken(t_xy *acpy, t_xy *b, t_fdf *fdf, int color)
+{
+	t_xy	a;
+	t_xy	tmp;
+
+	a.x = acpy->x;
+	a.y = acpy->y;
+	if (a.x == b->x)
+	{
+		while (a.y++ < b->y)
+			put_pixel(a.x, a.y - 1, color, fdf);
+		while (b->y++ < a.y)
+			put_pixel(b->x, b->y - 1, color, fdf);
+	}
+	else if (a.y == b->y)
+	{
+		while (a.x++ < b->x)
+			put_pixel(a.x - 1, a.y, color, fdf);
+		while (b->x++ < a.x)
+			put_pixel(b->x - 1, b->y, color, fdf);
+	}
+	else if (a.x < b->x)
+		while (a.x < b->x)
+		{
+			put_pixel(a.x, a.y, color, fdf);
+			tmp.y = a.y + (b->y - a.y) / (b->x - a.x);
+			a.x += 1;
+			while (a.y != tmp.y)
+			{
+				put_pixel(a.x, a.y, color, fdf);
+				if (a.y < tmp.y)
+					a.y++;
+				else
+					a.y--;
+			}
+			a.y = tmp.y;
+		}
+	else
+		while (b->x < a.x)
+		{
+			put_pixel(b->x, b->y, color, fdf);
+			tmp.y = b->y + (a.y - b->y) / (a.x - b->x);
+			b->x += 1;
+			while (b->y != tmp.y)
+			{
+				put_pixel(b->x, b->y, color, fdf);
+				if (b->y < tmp.y)
+					b->y++;
+				else
+					b->y--;
+			}
+			b->y = tmp.y;
+		}
+}
+
 void	apply_plan(t_xy *result, t_coord *coord, t_fdf *fdf)
 {
+	// This function is probably not working as excepted
 	result->x = coord->y * sin(fdf->y_factor * M_PI);
 	result->y = coord->y * cos(fdf->y_factor * M_PI);
 	result->x += coord->x * sin(fdf->x_factor * M_PI);
@@ -39,64 +147,6 @@ void	apply_plan(t_xy *result, t_coord *coord, t_fdf *fdf)
 	result->y *= fdf->zoom;
 }
 
-//X = x0*cos(a) - y0*sin(a)
-//Y = x0*sin(a) + y0*cos(a)
-
-void	trace_line(t_xy *acpy, t_xy *b, t_fdf *fdf)
-{
-	t_xy	a;
-	t_xy	tmp;
-
-	a.x = acpy->x;
-	a.y = acpy->y;
-	if (a.x == b->x)
-	{
-		while (a.y++ < b->y)
-			put_pixel(a.x, a.y - 1, 0x0000FF, fdf);
-		while (b->y++ < a.y)
-			put_pixel(b->x, b->y - 1, 0x0000FF, fdf);
-	}
-	else if (a.y == b->y)
-	{
-		while (a.x++ < b->x)
-			put_pixel(a.x - 1, a.y, 0x0000FF, fdf);
-		while (b->x++ < a.x)
-			put_pixel(b->x - 1, b->y, 0x0000FF, fdf);
-	}
-	else if (a.x < b->x)
-		while (a.x < b->x)
-		{
-			put_pixel(a.x, a.y, 0x0000FF, fdf);
-			tmp.y = a.y + (b->y - a.y) / (b->x - a.x);
-			a.x += 1;
-			while (a.y != tmp.y)
-			{
-				put_pixel(a.x, a.y, 0x0000FF, fdf);
-				if (a.y < tmp.y)
-					a.y++;
-				else
-					a.y--;
-			}
-			a.y = tmp.y;
-		}
-	else
-		while (b->x < a.x)
-		{
-			put_pixel(b->x, b->y, 0x0000FF, fdf);
-			tmp.y = b->y + (a.y - b->y) / (a.x - b->x);
-			b->x += 1;
-			while (b->y != tmp.y)
-			{
-				put_pixel(b->x, b->y, 0x0000FF, fdf);
-				if (b->y < tmp.y)
-					b->y++;
-				else
-					b->y--;
-			}
-			b->y = tmp.y;
-		}
-}
-
 void	trace_point(t_fdf *fdf, t_coord *coord)
 {
 	t_xy	result;
@@ -107,19 +157,39 @@ void	trace_point(t_fdf *fdf, t_coord *coord)
 	if (coord->right)
 	{
 		apply_plan(&tmp, coord->right, fdf);
-		trace_line(&result, &tmp, fdf);
+		trace_line(&result, &tmp, fdf, 0x0000FF);
 	}
 	if (coord->down)
 	{
 		apply_plan(&tmp, coord->down, fdf);
-		trace_line(&result, &tmp, fdf);
+		trace_line(&result, &tmp, fdf, 0x0000FF);
 	}
 }
 
 void	treatment(t_fdf *fdf)
 {
 	t_list	*list;
+	t_xy	center;
+	t_xy	z;
+	t_xy	y;
+	t_xy	x;
 
+	center.x = 100;
+	center.y = 100;
+	
+	z.x = 100;
+	z.y = 50;
+
+	y.x = 150;
+	y.y = 100;
+
+	x.x = 60;
+	x.y = 125;
+	
+	// Lets print the plan axes
+	trace_line(&center, &z, fdf, 0xFF0101);
+	trace_line(&center, &y, fdf, 0x01FF01);
+	trace_line(&center, &x, fdf, 0x0101FF);
 	list = fdf->coords;
 	while (list)
 	{
