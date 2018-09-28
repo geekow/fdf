c9b55afe27d54025aac7c8c4beb344985aaf9636
Jean Jacobi <jean.jacobi@live.de>
2018-09-28 17:44:12 +0200

    Init

--- srcs/key_parser.c
+++ srcs/key_parser.c
@@ -18,42 +18,42 @@ void	modify_zoom(t_fdf *fdf, char increase)
 {
 	if (increase && fdf->zoom < UINT_MAX)
 		fdf->zoom += 1;
-	else if (!increase && fdf->zoom > 0)
+	else if (!increase && fdf->zoom > 1)
 		fdf->zoom -= 1;
 }
 
 void	modify_offset(int keycode, t_fdf *fdf)
 {
 	if (keycode == TOP)
-		fdf->x_offset += (fdf->window_x / 16) / fdf->zoom;
+		fdf->x_offset += (int)(fdf->window_x / (fdf->zoom > 5 ? fdf->zoom : 5));
 	else if (keycode == BOTTOM)
-		fdf->x_offset -= (fdf->window_x / 16) / fdf->zoom;
+		fdf->x_offset -= (int)(fdf->window_x / (fdf->zoom > 5 ? fdf->zoom : 5));
 	else if (keycode == RIGHT)
-		fdf->y_offset += (fdf->window_y / 16) / fdf->zoom;
+		fdf->y_offset += (int)(fdf->window_y / (fdf->zoom > 5 ? fdf->zoom : 5));
 	else if (keycode == LEFT)
-		fdf->y_offset -= (fdf->window_y / 16) / fdf->zoom;
+		fdf->y_offset -= (int)(fdf->window_y / (fdf->zoom > 5 ? fdf->zoom : 5));
 }
 
 void	modify_axe(int keycode, t_fdf *fdf)
 {
 	if (keycode == NINE)
-		fdf->y_factor += 0.01;
+		fdf->y_factor += 0.1;
 	else if (keycode == THREE)
-		fdf->y_factor -= 0.01;
+		fdf->y_factor -= 0.1;
 	else if (keycode == EIGHT)
-		fdf->x_factor += 0.01;
+		fdf->x_factor += 0.1;
 	else if (keycode == TWO)
-		fdf->x_factor -= 0.01;
+		fdf->x_factor -= 0.1;
 	else if (keycode == SEVEN)
-		fdf->z_factor += 0.01;
+		fdf->z_factor += 0.1;
 	else if (keycode == ONE)
-		fdf->z_factor -= 0.01;
+		fdf->z_factor -= 0.1;
 	else if (keycode == FOUR)
-		fdf->z_factor = 1;
+		fdf->z_factor = 1.4;
 	else if (keycode == FIVE)
-		fdf->x_factor = 0.25;
+		fdf->x_factor = -1;
 	else if (keycode == SIX)
-		fdf->y_factor = 0.65;
+		fdf->y_factor = 3;
 }
 
 int		key_parser(int keycode, t_fdf *fdf)
@@ -61,19 +61,15 @@ int		key_parser(int keycode, t_fdf *fdf)
 	if (keycode == ESCAPE)
 		exit(0);
 	else if (keycode == PLUS || keycode == MINUS)
-		modify_zoom(fdf, keycode == PLUS ? 1 : 0);
+		modify_zoom(fdf, (char)(keycode == PLUS ? 1 : 0));
 	else if (ARROW_COND(keycode))
 		modify_offset(keycode, fdf);
 	else if (NBR_COND(keycode))
 		modify_axe(keycode, fdf);
 	else if (keycode == A)
-		fdf->show_axes = (fdf->show_axes) ? 0 : 1;
+		fdf->show_axes = (char)((fdf->show_axes) ? 0 : 1);
 	else if (keycode == E || keycode == R)
-		fdf->cam[2].z += (keycode == E ? 0.01 : -0.01);
-	else if (keycode == T || keycode == Y)
-		fdf->cam[1].y += (keycode == T ? 0.01 : -0.01);
-	else if (keycode == U || keycode == I)
-		fdf->cam[0].x += (keycode == U ? 0.01 : -0.01);
+		fdf->z_zoom += (keycode == E ? 0.1 : -0.1);
 	else
 		ft_printf("Unrecognized key pressed. Keycode: %i\n", keycode);
 	treatment(fdf);
